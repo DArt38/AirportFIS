30d3b4bd13d77d3564a5e48de8f787e11c7f5239
DArt38 <danielararat@unicomfacauca.edu.co>
2023-01-31 13:41:55 -0500

    folder frontend vue.js

---
+++ ConsumirApiVue/airportapi/src/router/index.js
@@ -0,0 +1,28 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import HomeView from '../views/HomeView.vue'
+import Dashboard from '../views/Dashboard.vue'
+
+Vue.use(VueRouter)
+
+const routes = [
+  {
+    path: '/',
+    name: 'home',
+    component: HomeView
+  },
+  {
+    path: '/dashboard',
+    name: 'Dashboard',
+    component: Dashboard
+  }
+
+]
+
+const router = new VueRouter({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes
+})
+
+export default router
