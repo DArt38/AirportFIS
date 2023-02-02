c6da80b765b76ada3da345fb1d243522e1f74385
DArt38 <danielararat@unicomfacauca.edu.co>
2023-02-01 22:27:51 -0500

    folder startup for clean code in program.cs

---
+++ WebAeropuerto/WebAeropuerto/Startup/DependencyInjectionSetup.cs
@@ -0,0 +1,15 @@
+﻿using WebAeropuerto.Data.Repositories;
+
+namespace WebAeropuerto.Startup
+{
+    public static class DependencyInjectionSetup
+    {
+        public static IServiceCollection RegisterServices(this IServiceCollection services)
+        {
+            services.AddScoped<ILlegadaRepository, LlegadaRepository>();
+            services.AddScoped<ISalidaRepository, SalidaRepository>();
+
+            return services;
+        }
+    }
+}
